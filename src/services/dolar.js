90d049cbc887d6e01aa896a793a9533b021c8974
Nelson Gonzalez <ngonzalez@mastransporte.cl>
2020-02-10 23:37:34 -0300

    quitando console y comentarios

--- src/services/dolar.js
+++ src/services/dolar.js
@@ -7,29 +7,23 @@ const headers = {
   host: 'api.sbif.cl'
 }
 
-//TODO: Move to util file
 export const getUrl = (url) => {
   return `${baseUrl}${url}`;
 }
 
-//TODO: Move to util file
 export const getUrlWithFormat = (url) => {
   return `${url}?apikey=${apikey}&formato=${format}`;
 }
 
 export const getDolarByRange = async (fromPeriod, fromDay, toPeriod, toDay) => {
-  console.log('2');
   const messageError = 'error al cargar data';
   try {
-    console.log('1');
     const url = getUrlWithFormat(`dolar/periodo/${fromPeriod}/dias_i/${fromDay}/${toPeriod}/dias_f/${toDay}`);  
     const response = await fetch(proxyurl + getUrl(url), {
       method : 'GET',
       headers
     });
-    console.log('response:', response);
     const data = await response.json();
-    console.log('data:', data);
     return (response.status === 200)? data : messageError;
   } catch (error) {
     console.log(error);
